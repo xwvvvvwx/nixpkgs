724f71953448fc9dfbd9c4af5134472d3d125187
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:34:53 +0100

    python: astral: 1.6.1 -> 1.7.1

--- pkgs/development/python-modules/astral/default.nix
+++ pkgs/development/python-modules/astral/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "astral";
-  version = "1.6.1";
+  version = "1.7.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ab0c08f2467d35fcaeb7bad15274743d3ac1ad18b5391f64a0058a9cd192d37d";
+    sha256 = "88086fd2006c946567285286464b2da3294a3b0cbba4410b7008ec2458f82a07";
   };
 
   propagatedBuildInputs = [ pytz requests ];
