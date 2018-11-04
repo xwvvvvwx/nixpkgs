4e07e37f6c46dd83f1086584b474fbc73ee2f01b
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2018-11-04 18:03:40 +0100

    python.pkgs.testscenarios: patch setup.cfg
    Without this patch, I get
    
        error: error in setup.cfg: command 'test' has no such option 'buffer'
        error: error in setup.cfg: command 'test' has no such option 'catch'

--- pkgs/development/python-modules/testscenarios/default.nix
+++ pkgs/development/python-modules/testscenarios/default.nix
@@ -13,6 +13,12 @@ buildPythonPackage rec {
     sha256 = "c257cb6b90ea7e6f8fef3158121d430543412c9a87df30b5dde6ec8b9b57a2b6";
   };
 
+  postPatch = ''
+    substituteInPlace setup.cfg \
+      --replace "buffer = 1" "" \
+      --replace "catch = 1" ""
+  '';
+
   propagatedBuildInputs = [ testtools ];
 
   meta = with stdenv.lib; {
