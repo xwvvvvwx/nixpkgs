f6d370a60808045d13fb61d0121caea697510c23
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:18 +0100

    python: testscenarios: 0.4 -> 0.5.0

--- pkgs/development/python-modules/testscenarios/default.nix
+++ pkgs/development/python-modules/testscenarios/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "testscenarios";
-  version = "0.4";
+  version = "0.5.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1671jvrvqlmbnc42j7pc5y6vc37q44aiwrq0zic652pxyy2fxvjg";
+    sha256 = "c257cb6b90ea7e6f8fef3158121d430543412c9a87df30b5dde6ec8b9b57a2b6";
   };
 
   propagatedBuildInputs = [ testtools ];
