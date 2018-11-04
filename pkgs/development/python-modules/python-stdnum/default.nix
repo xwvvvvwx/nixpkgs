7344fcf4e2a84b1d84434b3c6d239361493f39d2
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:35:14 +0100

    python: python-stdnum: 1.9 -> 1.10

--- pkgs/development/python-modules/python-stdnum/default.nix
+++ pkgs/development/python-modules/python-stdnum/default.nix
@@ -1,13 +1,13 @@
 { lib, fetchPypi, buildPythonPackage, isPy3k }:
 
 buildPythonPackage rec {
-  version = "1.9";
+  version = "1.10";
   pname = "python-stdnum";
   # Failing tests and dependency issue on Py3k
   disabled = isPy3k;
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d587a520182f9d8aef7659cca429f4382881589c8883a0a55322b2f94970bdb3";
+    sha256 = "caa050aed5d1d4825cc3294c99dc03d498aaf71f4415d659c9fcb58ff0303a5f";
   };
   meta = {
     homepage = https://arthurdejong.org/python-stdnum/;
