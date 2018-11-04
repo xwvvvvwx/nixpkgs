e48d022f622013ce06c8c3d178f64741152c89ae
Frederik Rietdijk <fridh@fridh.nl>
2018-11-04 11:34:55 +0100

    python: click-completion: 0.4.1 -> 0.5.0

--- pkgs/development/python-modules/click-completion/default.nix
+++ pkgs/development/python-modules/click-completion/default.nix
@@ -4,12 +4,12 @@
 
 buildPythonPackage rec {
   pname = "click-completion";
-  version = "0.4.1";
+  version = "0.5.0";
   disabled = (!isPy3k);
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1fjm22dyma26jrx4ki2z4dwbhcah4r848fz381x64sz5xxq3xdrk";
+    sha256 = "7600261cda0954b9794bd7afdaab35201be50a8c5404eaa4c0deb20086866c4c";
   };
 
   propagatedBuildInputs = [ click jinja2 shellingham six ];
